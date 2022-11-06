742fecf54e06ca8a8d3c84a7a47a22c3de0dd426
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-11-06 02:20:54 +0200

    complete task#1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
+
+let intervalId = null;
+
+refs.startButton.addEventListener('click', onStartClick);
+refs.stopButton.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  toggleButtom();
+  intervalId = setInterval(changeBgc, 1000);
+}
+
+function onStopClick() {
+  toggleButtom();
+  clearInterval(intervalId);
+}
+
+function toggleButtom() {
+  refs.startButton.toggleAttribute('disabled');
+  refs.stopButton.toggleAttribute('disabled');
+}
+
+function changeBgc() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
